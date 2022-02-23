1e4f5738133089eae0c04628281059550f85097f
David <Davidmaxsands@gmail.com>
2022-02-23 13:01:26 -0700

    commit tree 4bcc9d1ce6a0ef1c186a497807b508f7747a0cd1

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
 
-def countdown
+
+def countdown int
+    current_count = int
+    while current_count > 0
+        puts "#{current_count} SECOND(S)!"
+        current_count -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep int 
+    current_count = int
+    while current_count > 0
+        puts "#{current_count} SECOND(S)!"
+        current_count -= 1
+        sleep 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep(12)
